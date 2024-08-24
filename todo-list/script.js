b831a1a6ca8d1be6dbfaa76d5fe9f48009e55415
JinDevWS <jindev2024@gmail.com>
2024-08-24 23:42:04 +0900

    자바스크립트 투두리스트 수정

--- todo-list/script.js
+++ todo-list/script.js
@@ -10,18 +10,11 @@ const keyCodeCheck = function () {
     const liCheckbox = document.createElement("input");
     liCheckbox.type = "checkbox";
     liCheckbox.id = `check${newLi.id}`;
-    // liCheckbox.name = `check${inputValue}`;
 
     const checkLabel = document.createElement("label");
     checkLabel.htmlFor = `check${newLi.id}`;
-    // checkLabel.appendChild(liCheckbox);
-
-    // checkLabel.append(liCheckbox);
-    // checkLabel.append(inputValue);
 
     newLi.append(liCheckbox);
-    // newLi.append(inputValue);
-    // newLi.appendChild(liCheckbox).nextSibling = checkLabel;
     newLi.append(checkLabel);
     checkLabel.append(inputValue);
     todoList.appendChild(newLi);
